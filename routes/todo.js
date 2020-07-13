2620e046f5f90ac67010ca78463bbcdda221a3e1
aayush <iyush.bajracharya@gmail.com>
2020-07-13 17:29:54 +0545

    add create todo functionality

--- routes/todo.js
+++ routes/todo.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const router = express.Router();
+//require models
+const todo = require('../models/todo');
+const {
+    create
+} = require('../models/todo');
 
 router.get('/', function (req, res) {
-    res.render('todo');
+    todo.find({}, function (err, foundTodo) {
+        if (err) {
+            console.log("----error finding todo list" + err);
+        } else {
+            res.render('todo', {
+                todo: foundTodo
+            });
+        }
+    });
+});
+
+router.post('/', function (req, res) {
+    todo.create({
+        list: req.body.text
+    }, function (err, createdTodo) {
+        if (err) {
+            console.log("----error creating todo list----" + err);
+        } else {
+            res.redirect('/todo');
+        }
+    })
 });
 
 module.exports = router;
