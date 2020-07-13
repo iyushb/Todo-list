1158e361dc7c5c8ce023eccc2200f6acbb2558e2
aayush <iyush.bajracharya@gmail.com>
2020-07-13 16:54:07 +0545

    refactor routes separately

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+//require routes
+const todoRoutes = require('./routes/todo');
+const indexRoutes = require('./routes/index');
 
-app.get('/', function (req, res) {
-    res.send('homepage');
-})
+//database connection
+mongoose.connect('mongodb://localhost/todo_app', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log("----Database Connected----")).catch(err => console.log("DB connection error" + err.message));
+
+//setting view engine as ejs
+app.set("view engine", "ejs");
+
+app.use(indexRoutes);
+app.use('/todo', todoRoutes);
+
 
 app.listen(3000, function () {
-    console.log('server started');
+    console.log('----server started----');
 })
