0c10e3572eba7485d1a832ccc8cbce346c0cdc43
aayush <iyush.bajracharya@gmail.com>
2020-07-17 15:20:24 +0545

    create register form and get route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const user = require('../models/user');
 const router = express.Router();
 
 router.get('/', function (req, res) {
     res.send('homepage');
+});
+
+router.get('/register', function (req, res) {
+    res.render('register');
 });
 
 module.exports = router;
