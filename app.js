214e646acfea247f156293edc300f035144ea460
iyushb <51288133+iyushb@users.noreply.github.com>
2021-05-15 11:03:12 +0545

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 //database connection
-mongoose.connect('mongodb+srv://aayushDB:62403502@hsuyaa@cluster0.03pcw.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://<username>:<password>@hsuyaa@cluster0.03pcw.mongodb.net/<dbname>?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: true
